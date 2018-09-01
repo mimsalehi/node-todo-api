acb7f7737cd8b328fb9c103847c1d162c4fe46ca
memolroxo <masoudline@gmail.com>
2018-09-01 12:56:58 +0430

    Add DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -100,3 +100,41 @@ describe("GET /todos/:id", ()=>{
             .end(done)
     })
 })
+
+describe("DELETE /todos/:id", ()=>{
+    it("Should remove a todo", (done)=>{
+        var hexId = todos[0]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body._id).toBe(hexId)
+            })
+            .end((err, res)=>{
+                if(err){
+                    return done(err)
+                }
+
+                Todo.findById(hexId).then((todo)=>{
+                    expect(todo).toBeNull();
+                    done();
+                })
+
+            })
+    })
+    it("Should return 404 if todo not found", (done)=>{
+        var hexId = new ObjectID().toHexString();
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(404)
+            .end(done)
+    })
+
+    it("Should return for non-object ids", (done)=>{
+        request(app)
+            .delete(`/todos/123klj`)
+            .expect(404)
+            .end(done)
+    })
+})
