c3c04fa537d2f1599ba204d74235d7a8bf28d40c
memolroxo <masoudline@gmail.com>
2018-09-04 11:09:41 +0430

    Add login and logout routes

--- server/models/user.js
+++ server/models/user.js
@@ -48,6 +48,15 @@ UserSchema.methods.generateAuthToken = function(){
         return token
     })
 }
+UserSchema.methods.removeToken = function(token){
+    var user = this;
+
+    return user.update({
+        $pull: {
+            tokens: {token}
+        }
+    })
+}
 UserSchema.statics.findByToken = function(token) {
     var User = this;
     var decoded;
@@ -64,6 +73,27 @@ UserSchema.statics.findByToken = function(token) {
         'tokens.access': 'auth'
     })
 }
+
+UserSchema.statics.findByCredentials = function(email, password){
+    var User = this;
+
+    return User.findOne({email}).then((user)=>{
+        if(!user){
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject)=>{
+            bcrypt.compare(password, user.password, (err, res)=>{
+                if(res){
+                    resolve(user)
+                }else{
+                    reject();
+                }
+            })
+        })
+    })
+}
+
 UserSchema.pre('save', function(next){
     var user = this;
     if(user.isModified('password')){
@@ -77,6 +107,7 @@ UserSchema.pre('save', function(next){
         next();
     }
 })
+
 var User = mongoose.model('User', UserSchema)
 
 module.exports = {User}
--- server/server.js
+++ server/server.js
@@ -99,6 +99,29 @@ app.get('/user/me', authenticate, (req, res)=>{
     res.send(req.user)
 })
 
+// Login Route
+
+app.post('/users/login', (req, res)=>{
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user)=>{
+        return user.generateAuthToken().then((token)=>{
+            res.header('x-auth', token).send(user)
+        })
+    }).catch((err)=>{
+        res.status(400).send()
+    })
+})
+
+// Logout Route
+app.delete('/users/me/token', authenticate, (req, res)=>{
+    req.user.removeToken(req.token).then(()=>{
+        res.status(200).send();
+    }).catch((err)=>{
+        res.status(400).send();
+    })
+})
+
 // POST /users
 
 app.post('/users', (req, res)=>{
